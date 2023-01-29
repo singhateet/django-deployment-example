f426a2b3f074f45694c8f2a7989969a72f771b4e
singhateet <ateet.286@gmail.com>
2023-01-29 18:18:08 +0530

    first commit

--- django_website/liveCandleStick/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('chart/', views.chart_view, name='chart_view'),
-]
--- django_website/uploadData/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('uploadData/', views.upload_csv, name='upload_csv'),
-]
